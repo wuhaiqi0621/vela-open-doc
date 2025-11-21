b9673569cd8a329ec7289503cc3325875c1a4bae
Zaona <3409473369@qq.com>
2025-11-21 11:26:01 +0800

    feat: 添加中文翻译

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Vela Open Doc",
   description: "Community-Created Xiaomi Vela Document",
+  lang: 'zh-CN',
   outDir: './dist',
   cleanUrls: true,
   head: [
@@ -11,9 +12,18 @@ export default defineConfig({
   ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    logo: '/logo.svg',
+    siteTitle: 'Vela Open Doc',
+    sidebarMenuLabel: '菜单',
+    returnToTopLabel: '返回顶部',
+    outlineTitle: '本页目录',
+    darkModeSwitchLabel: '外观',
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '首页', link: '/' }
     ],
 
     sidebar: [
@@ -39,24 +49,26 @@ export default defineConfig({
     search: {
       provider: 'local',
       options: {
-        locales: {
-          zh: {
-            translations: {
-              button: {
-                buttonText: '搜索文档',
-                buttonAriaLabel: '搜索文档'
-              },
-              modal: {
-                noResultsText: '无法找到相关结果',
-                resetButtonTitle: '清除查询条件',
-                footer: {
-                  selectText: '选择',
-                  navigateText: '切换'
-                }
-              }
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            searchBoxPlaceholder: '搜索文档',
+            resetButtonTitle: '清除查询条件',
+            closeButtonAriaLabel: '关闭搜索',
+            noResultsText: '无法找到相关结果',
+            footer: {
+              selectText: '选择',
+              selectTextAriaLabel: '选择',
+              navigateText: '导航',
+              navigateTextAriaLabel: '导航到结果',
+              closeText: '关闭',
+              closeTextAriaLabel: '关闭搜索对话框'
             }
           }
-        }
+        },
       }
     }
   }
